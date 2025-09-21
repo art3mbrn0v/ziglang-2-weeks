7a950540e5e150ee621eee8781ecf3d654a80682
art3mbrn0v <empty>
2025-09-21 07:41:31 -0400

    integer u7

---
+++ integer-u7.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+pub fn main() void {
+    const a: u7 = 100;
+    const b: u7 = 20;
+
+    const sum: u7 = a + b; // 120, 0  - 127:  error: overflow of integer type 'u7' with value '130'
+    std.debug.print("Sum u7: {d}\n", .{sum});
+}
